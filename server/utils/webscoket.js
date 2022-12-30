dd51a7496e266448e67fea0ab8808851ff51896e
loveverse <398096830@qq.com>
2022-12-30 18:35:21 +0800

    增加websocket

---
+++ server/utils/webscoket.js
@@ -0,0 +1,11 @@
+const WebSocketServer = require("ws").Server;
+const { APP_PORT } = require("../config/index");
+const wss = new WebSocketServer({ port: APP_PORT });
+
+wss.on("connection", function (ws) {
+  ws.on("message", function (message) {
+    const obj = JSON.parse(message);
+    console.log("[ obj ] >", obj);
+  });
+  ws.send("send");
+});

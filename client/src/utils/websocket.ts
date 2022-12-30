6caf1bdd7697c1429e8680cda3e18168efdb01a5
loveverse <398096830@qq.com>
2022-12-30 18:34:38 +0800

    增加websocket

--- client/src/utils/websocket.ts
+++ client/src/utils/websocket.ts
@@ -1,5 +1,41 @@
 const url: string = "ws://localhost: 40001";
-const webScoket = new WebSocket(url);
-webScoket.onopen = function () {
-  console.log("[ 11 ] >", 11);
-};
+
+let websocket: any = null;
+let global_callback: any = null;
+function createWebSocket(callback: Function) {
+  if (websocket === null || typeof websocket !== WebSocket) {
+    initWebSocket(callback);
+  }
+}
+
+function initWebSocket(callback: Function) {
+  global_callback = callback;
+  websocket = new WebSocket(url);
+  websocket.onmessage = function (e) {
+    websocketOnMessage(e);
+  };
+  websocket.onclose = function (e) {
+    websocketClose(e);
+  };
+  websocket.onopen = function (e) {
+    websocketOpen(e);
+  };
+  websocket.onerror = function (e) {
+    console.log("webSocket连接发生错误");
+  };
+}
+// 数据接收
+function websocketOnMessage(msg) {}
+function websocketClose(e: any) {
+  console.log("连接关闭", e.code);
+}
+function websocketOpen(e) {
+  console.log("连接打开");
+}
+
+// 实际调用的方法
+function sendSocket(data){
+  if(websocket.readyState === websocket.open){
+    
+  }
+}

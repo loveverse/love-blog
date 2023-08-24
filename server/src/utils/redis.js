32b21bae26d4cfd030743717fe468794b9a9c990
loveverse <398096830@qq.com>
2023-08-24 10:51:43 +0800

    修改配置

--- server/src/utils/redis.js
+++ server/src/utils/redis.js
@@ -3,29 +3,26 @@ const {
   APP_REDIS_HOST,
   APP_REDIS_PORT,
   APP_REDIS_PASSWORD,
-  APP_REDIS_DB
+  APP_REDIS_DB,
 } = require("../config");
 
-if (!process.env.NODE_ENV) {
-  const client = Redis.createClient({
-    url: "redis://" + APP_REDIS_HOST + ":" + APP_REDIS_PORT,
-    host: APP_REDIS_HOST,
-    port: APP_REDIS_PORT,
-    password: APP_REDIS_PASSWORD,
-    database: APP_REDIS_DB,
-  });
+const client = Redis.createClient({
+  url: "redis://" + APP_REDIS_HOST + ":" + APP_REDIS_PORT,
+  host: APP_REDIS_HOST,
+  port: APP_REDIS_PORT,
+  password: APP_REDIS_PASSWORD,
+  database: APP_REDIS_DB,
+});
 
-  client.connect();
-  client.on("connect", () => {
-    console.log("Redis client connected");
-  });
-  // 连接错误处理
-  client.on("error", (err) => {
-    console.error(err);
-  });
-} else {
-  client = null;
-}
+client.connect();
+client.on("connect", () => {
+  console.log("Redis client connected");
+});
+// 连接错误处理
+client.on("error", (err) => {
+  console.error(err);
+});
+
 // client.set("age", 1);
 
 module.exports = client;
--- server/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pm2 restart ./app.js --watch
